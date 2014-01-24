1158d01db3bfe8e45f67219b61455a334f64ab10
Michael Parrott <michael.parrott94@gmail.com>
2014-01-24 18:40:52 -0500

    DB test

---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+    $dbconn = pg_connect("host=ec2-54-197-241-91.compute-1.amazonaws.com port=5432 dbname=d65t2it35j2n1v user=qbvtfhdthkmlmu password=8kv-mljjzibMSa3fL9KcHDDAcB")
+
+    echo $dbconn
+?>
