14e92986a6298106eed397e9adbd476d21725406
unknown <au.andy.ch@gmail.com>
2014-01-25 04:19:11 -0500

    osjda

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <html>
-<head>
-	<title>Test123</title>
-</head>
-<body>
-	<div id="fb-root"></div>
+ <head>
+	 <meta charset="utf-8">
+	 <title>CAP-IT</title>
+	 <link rel="stylesheet" href="http://flip.hr/css/bootstrap.min.css">
+ </head>
+
+ <body style="background:#4C4646">
+ 	<div id="fb-root"></div>
 	<script src="http://code.jquery.com/jquery-2.1.0.js"></script>
 	<script src="login.js"></script>
-<fb:login-button show-faces="true" width="200" max-rows="1" scope="user_photos"></fb:login-button>
-<button class="btn1">Go!!</button>
+	<div class="container">
+	<h1 style="margin-left:355px; width:580px; margin-top:300px; margin-left:-50px; color:white"><big><big><big>JUST CAP-IT DOE</big></big></big></h1>
+		<div class="nav-container horizontal" style="position:relative; 
+													width:200px;
+													margin-left:320px; 
+													margin-top:0px;">
+			<div class="link_link" id="menu_0" style="text-align:right">
+				<a href="http://cap-it.herokuapp.com/" target="" name="benhulse"  style="color:white">New Game</a>
+			</div>										
+			<div class="link_link" id="menu_1" style="text-align:right">
+				<a href="http://benhulse.com/filter/design" target="" name="benhulse"  style="color:white">Home</a>
+			</div>
+			<div class="link_link" id="menu_2" style="text-align:right">
+				<a href="http://benhulse.com/filter/design" target="" name="benhulse" style="color:white">View Previous</a>
+			</div>
+			<div class="link_link" id="menu_3" style="text-align:right">
+				<a href="http://benhulse.com/filter/design" target="" name="benhulse"  style="color:white">About Cap-It</a>
+			</div>
+		</div>
+		<big><big><p style="color:WHITE;
+			position:relative;
+			width:550px;
+			margin-top:-110px;
+			margin-left:535px;">Words with friends, draw my thing, and cards against humanity just had a baby.</p></big></big>
+	</div>
+	<fb:login-button show-faces="true" width="200" max-rows="1" scope="user_photos"></fb:login-button>
 </body>
+
 </html>
