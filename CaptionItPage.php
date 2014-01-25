fbf6eede655e05880de0cedc9ab45b0e99185d3e
Michael Parrott <michael.parrott94@gmail.com>
2014-01-25 12:13:18 -0500

    find cause of problem

--- CaptionItPage.php
+++ CaptionItPage.php
@@ -16,22 +16,7 @@
  <script src="http://code.jquery.com/jquery-2.1.0.js"></script>
  <script src="main.js"></script>
  
-          <script>
-            // this is the id of the submit button
-            $("#submitButton").click(function() {
-                alert("WOW");
-                var url = "services.php"; // the script where you handle the form input.
-                alert($("#captionSubmit").serialize());
-                $.post(url,
-                   $("#captionSubmit").serialize(), // serializes the form's elements.
-                   function(data)
-                   {
-                    alert(data);
-                   });
-
-                return false; // avoid to execute the actual submit of the form.
-            });
-         </script>
+          <
 
  <h1 style="margin-left:355px; margin-top:100px; color:white"><big><big><big>JUST CAP-IT DOE</big></big></big></h1>
  <div class="nav-container horizontal" style="margin-left:360px; margin-top: 0px">
@@ -259,6 +244,19 @@
 			
 		</div>
 	</div>
+	<script>
+        // this is the id of the submit button
+        $("#submitButton").click(function() {
+            var url = "services.php"; // the script where you handle the form input.
+            $.post(url,
+               $("#captionSubmit").serialize(), // serializes the form's elements.
+               function(data)
+               {
+               });
+
+            return false; // avoid to execute the actual submit of the form.
+        });
+     </script>
  </body>
  </html>
  
