b4e5200153c6d95196e4f863eed5b6f2a026388d
unknown <au.andy.ch@gmail.com>
2014-01-24 22:46:55 -0500

    Andy

--- login.js
+++ login.js
@@ -27,7 +27,8 @@ window.fbAsyncInit = function() {
 		 $.each(response.images, function(idx, obj) {
 			 console.log(obj.source);
 			 showImage(response.data.url, 500, 500, 'You look so cool!');
-	});  
+		});  
+	}
 }
  
 function showImage(src, width, height, alt) {
