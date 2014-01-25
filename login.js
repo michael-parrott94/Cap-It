28a44ec04592aa7e0b95318036c7e672d56d5e61
unknown <au.andy.ch@gmail.com>
2014-01-24 23:54:41 -0500

    Andy

--- login.js
+++ login.js
@@ -23,10 +23,10 @@ window.fbAsyncInit = function() {
       window.alert("WHAT UP! " + response.name + "!");
     });
 
-	FB.api("/fql?q=SELECT%20src_big%20FROM%20photo%20WHERE%20pid%20IN%20%28SELECT%20pid%20FROM%20photo_tag%20WHERE%20subject%3Dme%28%29%20ORDER%20BY%20created%20ASC%29%20LIMIT%2010",  function(response) {
-		 $.each(response, function(idx, obj) {
-			 console.log(obj.src_big);
-			 showImage(obj.src_big, 500, 500, 'You look so cool!');
+	FB.api('/fql?q=SELECT%20src_big%20FROM%20photo%20WHERE%20pid%20IN%20%28SELECT%20pid%20FROM%20photo_tag%20WHERE%20subject%3Dme%28%29%20ORDER%20BY%20created%20ASC%29%20LIMIT%201',  function(response) {
+		 //$.each(response, function(idx, obj) {
+			 //console.log(obj.src_big);
+			 showImage(response.data.src_big, 500, 500, 'You look so cool!');
 		});  
 	});
 }
