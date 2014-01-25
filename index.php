0c838b6b19666062d6b69e2e85815521d106e8c8
unknown <au.andy.ch@gmail.com>
2014-01-24 23:09:23 -0500

    Andy - facebook stuff

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 	<div id="fb-root"></div>
 	<script src="http://code.jquery.com/jquery-2.1.0.js"></script>
 	<script src="login.js"></script>
+<fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>
 </body>
 </html>
