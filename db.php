c29d35d226670e3a7c92bae6b21805d453491d61
Michael Parrott <michael.parrott94@gmail.com>
2014-01-24 20:03:29 -0500

    DB test 3

--- db.php
+++ db.php
@@ -1,3 +1,4 @@
+<h1>top</h1>
 <?php
     $dbconn = pg_connect("host=ec2-54-197-241-91.compute-1.amazonaws.com port=5432 dbname=d65t2it35j2n1v user=qbvtfhdthkmlmu password=8kv-mljjzibMSa3fL9KcHDDAcB") or die('connection failure')
 
