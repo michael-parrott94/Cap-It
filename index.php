061204725992e84e2c25ea0e9918c83d48094495
unknown <au.andy.ch@gmail.com>
2014-01-24 19:11:21 -0500

    Andy - removed login.html

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-MICHAEL PARROTT IS A POOSE!
+<!DOCTYPE html>
+<html>
+<body>
+	
+	<div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1401062393477841',
+          status     : true,
+          xfbml      : true
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+    </script>
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
+    
+	
