5f2616f6518aba64356abd17ad506a4a16284f90
Michael Parrott <michael.parrott94@gmail.com>
2014-01-25 11:36:54 -0500

    Stop refresh on caption

--- CaptionItPage.php
+++ CaptionItPage.php
@@ -8,7 +8,15 @@
          
          <link rel="stylesheet" href="styles.css">
          
-         <script>
+
+ </head>
+ 
+ <body style="background:#4C4646">
+ <div id="fb-root"></div>
+ <script src="http://code.jquery.com/jquery-2.1.0.js"></script>
+ <script src="main.js"></script>
+ 
+          <script>
             // this is the id of the submit button
             $("#submitButton").click(function() {
 
@@ -24,13 +32,6 @@
                 return false; // avoid to execute the actual submit of the form.
             });
          </script>
- </head>
- 
- <body style="background:#4C4646">
- <div id="fb-root"></div>
- <script src="http://code.jquery.com/jquery-2.1.0.js"></script>
- <script src="main.js"></script>
- 
 
  <h1 style="margin-left:355px; margin-top:100px; color:white"><big><big><big>JUST CAP-IT DOE</big></big></big></h1>
  <div class="nav-container horizontal" style="margin-left:360px; margin-top: 0px">
