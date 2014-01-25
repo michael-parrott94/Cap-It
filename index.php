22c64aed4220061a9605572a04918c1a777d6ba6
unknown <au.andy.ch@gmail.com>
2014-01-24 19:35:33 -0500

    Andy - facebook login

--- index.php
+++ index.php
@@ -1,23 +1,51 @@
 <!DOCTYPE html>
 <html>
-<title>TESTING</title>
+<head>
+	<title>TESTING</title>
+</head>
 <body>
 	<div id="fb-root"></div>
     <script>
-      window.fbAsyncInit = function() {
+     window.fbAsyncInit = function() {
         FB.init({
           appId      : '1401062393477841',
           status     : true,
           xfbml      : true
         });
-      };
+    };
 
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/all.js";
-         fjs.parentNode.insertBefore(js, fjs);
+	function facebookLogin() {
+ 
+		FB.getLoginStatus(function(response) {
+ 
+			if (response.status === 'connected') {
+				// connected
+				getProfileImage();
+			} else if (response.status === 'not_authorized') {
+				//app not_authorized
+				FB.login(function(response) {
+					if (response && response.status === 'connected') {
+						getProfileImage();
+					}
+				});
+			} else {
+				// not_logged_in to Facebook
+				FB.login(function(response) {
+					if (response && response.status === 'connected') {
+						getProfileImage();
+					}
+				});
+			}
+		}); 
+	}	
+
+	  
+    (function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
     </script>
 <h1>My First Heading</h1>
