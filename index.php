6fcbfa1f68e3d71c8848febb40d7d337f8cca11e
unknown <au.andy.ch@gmail.com>
2014-01-24 20:37:22 -0500

    Dont worry

--- index.php
+++ index.php
@@ -5,42 +5,40 @@
 <body>
 <div id="fb-root"></div>
 <script>
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1401062393477841',
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
 
-  window.alert("sometext");
-  // window.fbAsyncInit = function() {
-  // FB.init({
-    // appId      : '1401062393477841',
-    // status     : true, // check login status
-    // cookie     : true, // enable cookies to allow the server to access the session
-    // xfbml      : true  // parse XFBML
-  // });
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    if (response.status === 'connected') {
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      FB.login();
+    } else {
+      FB.login();
+    }
+  });
+  };
 
-  // FB.Event.subscribe('auth.authResponseChange', function(response) {
-    // if (response.status === 'connected') {
-      // testAPI();
-    // } else if (response.status === 'not_authorized') {
-      // FB.login();
-    // } else {
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
 
-      // FB.login();
-    // }
-  // });
-  // };
-
-  // (function(d){
-   // var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   // if (d.getElementById(id)) {return;}
-   // js = d.createElement('script'); js.id = id; js.async = true;
-   // js.src = "//connect.facebook.net/en_US/all.js";
-   // ref.parentNode.insertBefore(js, ref);
-  // }(document));
-
-  // function testAPI() {
-    // console.log('Welcome!  Fetching your information.... ');
-    // FB.api('/me', function(response) {
-      // console.log('Good to see you, ' + response.name + '.');
-    // });
-  // }
+  function testAPI() {
+  	window.alert("WORKING");
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  }
 </script>
 
 <!--
