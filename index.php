af8eaea988e7f4d2a3f64fbaffdace15fbc38509
unknown <au.andy.ch@gmail.com>
2014-01-24 21:02:58 -0500

    Andy facebook login

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
 
   FB.Event.subscribe('auth.authResponseChange', function(response) {
     if (response.status === 'connected') {
-      testAPI();
+	// connected
+        getProfileImage();
     } else if (response.status === 'not_authorized') {
       FB.login();
     } else {
@@ -23,22 +24,38 @@
     }
   });
   };
+  
+  function getProfileImage() {
+    var $photo = $('.photo'),
+        $btn = $('.btn-fb'),
+        $fbPhoto = $('img.fb-photo');
+		
+    //uploading
+    $btn.text('Uploading...');
+ 
+    FB.api("/me/picture?width=180&height=180",  function(response) {
+        var profileImage = response.data.url.split('https://')[1], //remove https to avoid any cert issues
+            randomNumber = Math.floor(Math.random()*256);
+        //remove if there and add image element to dom to show without refresh
+        if( $fbPhoto.length ){
+            $fbPhoto.remove();
+        }
+        //add random number to reduce the frequency of cached images showing
+        $photo.append('<img class=\"fb-photo img-polaroid\" src=\"http://' + profileImage + '?' + randomNumber + '\">');
+        $btn.addClass('hide');
+    }); 
+}
+
 
   (function(d){
    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
    if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
+   js = d.createElement('script'); 
+   js.id = id; 
+   js.async = true;
    js.src = "//connect.facebook.net/en_US/all.js";
    ref.parentNode.insertBefore(js, ref);
   }(document));
-
-  function testAPI() {
-  	window.alert("WORKING");
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  }
 </script>
 
 <!--
