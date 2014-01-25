64c5da69b87afa2c0e1e08d60ed8bfa6d54799a5
unknown <au.andy.ch@gmail.com>
2014-01-24 20:02:20 -0500

    Andy more stuff

--- index.php
+++ index.php
@@ -1,81 +1,70 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title>TESTING</title>
-</head>
+<head></head>
 <body>
-	<div id="fb-root"></div>
-    <script>
-     window.fbAsyncInit = function() {
-        FB.init({
-			appId      : '1401062393477841',
-			channelUrl: '//cap-it.herokuapp.com/channel.html', // Channel File
-			status: true, // check login status
-			cookie: true, // enable cookies to allow the server to access the session
-			xfbml: true // parse XFBML
-        });
-    };
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '{1401062393477841}',
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
 
-	function facebookLogin() {
- 
-		FB.getLoginStatus(function(response) {
- 
-			if (response.status === 'connected') {
-				// connected
-				getProfileImage();
-			} else if (response.status === 'not_authorized') {
-				//app not_authorized
-				FB.login(function(response) {
-					if (response && response.status === 'connected') {
-						getProfileImage();
-					}
-				});
-			} else {
-				// not_logged_in to Facebook
-				FB.login(function(response) {
-					if (response && response.status === 'connected') {
-						getProfileImage();
-					}
-				});
-			}
-		}); 
-	}	
+  // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+  // for any authentication related change, such as login, logout or session refresh. This means that
+  // whenever someone who was previously logged out tries to log in again, the correct case below 
+  // will be handled. 
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    // Here we specify what we do with the response anytime this event occurs. 
+    if (response.status === 'connected') {
+      // The response object is returned with a status field that lets the app know the current
+      // login status of the person. In this case, we're handling the situation where they 
+      // have logged in to the app.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // In this case, the person is logged into Facebook, but not into the app, so we call
+      // FB.login() to prompt them to do so. 
+      // In real-life usage, you wouldn't want to immediately prompt someone to login 
+      // like this, for two reasons:
+      // (1) JavaScript created popup windows are blocked by most browsers unless they 
+      // result from direct interaction from people using the app (such as a mouse click)
+      // (2) it is a bad experience to be continually prompted to login upon page load.
+      FB.login();
+    } else {
+      // In this case, the person is not logged into Facebook, so we call the login() 
+      // function to prompt them to do so. Note that at this stage there is no indication
+      // of whether they are logged into the app. If they aren't then they'll see the Login
+      // dialog right after they log in to Facebook. 
+      // The same caveats as above apply to the FB.login() call here.
+      FB.login();
+    }
+  });
+  };
 
-	function getProfileImage() {
-		var $photo = $('.photo'),
-			$btn = $('.btn-fb'),
-			$fbPhoto = $('img.fb-photo');
-	 
-		//uploading
-		$btn.text('Uploading...');
-	 
-		FB.api("/me/picture?width=180&height=180",  function(response) {
-	 
-			var profileImage = response.data.url.split('https://')[1], //remove https to avoid any cert issues
-				randomNumber = Math.floor(Math.random()*256);
-	 
-		   //remove if there and add image element to dom to show without refresh
-		   if( $fbPhoto.length ){
-			   $fbPhoto.remove();
-		   }
-			 //add random number to reduce the frequency of cached images showing
-		   $photo.append('<img class=\"fb-photo img-polaroid\" src=\"http://' + profileImage + '?' + randomNumber + '\">');
-			$btn.addClass('hide');
-		}); 
-	}
+  // Load the SDK asynchronously
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
 
-    // Load the SDK Asynchronously
-	(function (d) {
-		var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-		if (d.getElementById(id)) { return;}
-		js = d.createElement('script');
-		js.id = id;
-		js.async = true;
-		js.src = "//connect.facebook.net/en_US/all.js";
-		ref.parentNode.insertBefore(js, ref);
-	}(document));
-    </script>
+  // Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  }
+</script>
+
+<!--
+  Below we include the Login Button social plugin. This button uses the JavaScript SDK to
+  present a graphical Login button that triggers the FB.login() function when clicked. -->
+
+<fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>
 </body>
 </html>
-    
-	
