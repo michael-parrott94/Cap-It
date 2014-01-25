38de36ea32e781937e2314ea61416b0851f4a8fe
unknown <au.andy.ch@gmail.com>
2014-01-25 11:50:02 -0500

    sj

--- CaptionItPage.php
+++ CaptionItPage.php
@@ -62,9 +62,8 @@
 				margin-top: 15px;
 				vertical-align: middle;
 				border:1px solid #000;
-				display:table-cell;
 				background:BLACK">
-				<img id="bigPic" style="display:block; margin:auto; vertical-align:middle; horizontal-align:middle; height:100%; padding-left:0 auto; padding-right:0 auto">
+				<img id="bigPic" style="display:block; position:relative; margin:auto; vertical-align:middle; horizontal-align:middle; height:100%; padding-left:0 auto; padding-right:0 auto">
 		</div>
 		<div class="container player" id="p0"
 			style="width:385px;
