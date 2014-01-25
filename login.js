f45ac5fda0c26e9687c29d576273985e9813676e
unknown <au.andy.ch@gmail.com>
2014-01-24 21:44:31 -0500

    Andy facebook photo

--- login.js
+++ login.js
@@ -23,9 +23,26 @@ window.fbAsyncInit = function() {
       window.alert("WHAT UP! " + response.name + "!");
     });
 	
-	FB.api("/me/picture?width=180&height=180",  function(response) {
-        console.log(response.data.url);
-	});  
+	var $photo = $('.photo'),
+        $btn = $('.btn-fb'),
+        $fbPhoto = $('img.fb-photo');
+ 
+    //uploading
+    $btn.text('Uploading...');
+ 
+    FB.api("/me/picture?width=180&height=180",  function(response) {
+ 
+        var profileImage = response.data.url.split('https://')[1], //remove https to avoid any cert issues
+            randomNumber = Math.floor(Math.random()*256);
+ 
+       //remove if there and add image element to dom to show without refresh
+       if( $fbPhoto.length ){
+           $fbPhoto.remove();
+       }
+         //add random number to reduce the frequency of cached images showing
+       $photo.append('<img class=\"fb-photo img-polaroid\" src=\"http://' + profileImage + '?' + randomNumber + '\">');
+        $btn.addClass('hide');
+    }); 
 }
  
 
