554730c966ce8fa74b284008b4974e6c9422c85c
unknown <au.andy.ch@gmail.com>
2014-01-25 11:49:18 -0500

    tasd

--- CaptionItPage.php
+++ CaptionItPage.php
@@ -62,8 +62,9 @@
 				margin-top: 15px;
 				vertical-align: middle;
 				border:1px solid #000;
+				display:table-cell;
 				background:BLACK">
-				<img id="bigPic" style="display:block; margin-left:0 auto; margin-right:0 auto; vertical-align:middle; horizontal-align:middle; height:100%; padding-left:0 auto; padding-right:0 auto">
+				<img id="bigPic" style="display:block; margin:auto; vertical-align:middle; horizontal-align:middle; height:100%; padding-left:0 auto; padding-right:0 auto">
 		</div>
 		<div class="container player" id="p0"
 			style="width:385px;
