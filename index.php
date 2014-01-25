ae0fff79d0d27cd69ac8193f95bc618e4ff45723
unknown <au.andy.ch@gmail.com>
2014-01-24 20:31:04 -0500

    Andy more stuff

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <html>
 <head>
 	<title>Test123</title>
-	function printjs($script) {
-		print "\n\n <script language='javascript'> \n\n $script \n\n script>\n\n";
-	}
 </head>
 <body>
 <div id="fb-root"></div>
@@ -26,7 +23,7 @@
       // The response object is returned with a status field that lets the app know the current
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
-	  printjs("alert('IT WORKS!');");
+	  window.alert("sometext");
       testAPI();
     } else if (response.status === 'not_authorized') {
       // In this case, the person is logged into Facebook, but not into the app, so we call
