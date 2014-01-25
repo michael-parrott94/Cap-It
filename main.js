333850e6ca920427f1468960c25cfadd5f29a6f9
unknown <au.andy.ch@gmail.com>
2014-01-25 08:06:07 -0500

    s

--- main.js
+++ main.js
@@ -20,21 +20,7 @@ window.fbAsyncInit = function() {
 
 function myLooper()
 {
-	$.post("services.php",
-	{
-		user : "all",
-	},function(response)
-	{
-		console.log("Grabbing all the user data in the looper.");
-		var parsedResponse = $.parseJSON(response);
-		for (var i = 0; i < parsedResponse.length; i++)
-		{
-			$("#p" + i + " #name").text(parsedResponse[i].name);
-			$("#p" + i + " img").attr("src", parsedResponse[i].fb_pp);
-			$("#p" + i + " img").attr("height", "85px");
-			$("#p" + i + " img").attr("width", "85px");
-		}
-	});
+
 }
 
 function Initialize() {
@@ -62,12 +48,26 @@ function Initialize() {
 				console.log("-----------------------");
 				console.log("AddUser Response: " + response);
 				
-				
+				$.post("services.php",
+				{
+					user : "all",
+				},function(response)
+				{
+					console.log("Grabbing all the user data in the looper.");
+					var parsedResponse = $.parseJSON(response);
+					for (var i = 0; i < parsedResponse.length; i++)
+					{
+						$("#p" + i + " #name").text(parsedResponse[i].name);
+						$("#p" + i + " img").attr("src", parsedResponse[i].fb_pp);
+						$("#p" + i + " img").attr("height", "85px");
+						$("#p" + i + " img").attr("width", "85px");
+					}
+				});
 			});
 		});
     });
     
-    var looper = setInterval(myLooper, 5000);
+    //var looper = setInterval(myLooper, 5000);
 }
 
 
