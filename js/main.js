2651826c3c664730b13098fc169c1efdb8f0ca9f
Andy Au <andyau@ubuntu.(none)>
2014-02-16 15:18:19 -0500

    Andy added score updating functionality

--- js/main.js
+++ js/main.js
@@ -34,12 +34,12 @@ $(document).ready(function() {
 		var score = parseInt($('#' + id + ' #score').text()) + 10;
 		window.alert($('#' + id + ' #name').text() + ' gets 10 points! \n He now has ' 
 			+ score + ' points!');
-		/*
+
 		$.post("services.php",
 		{
 			'score': score,
-			'fb_id': 
-		} */
+			'fb_id': playerFBIds[id]
+		} 
 	});
 
 	// Main looper for every second
@@ -190,9 +190,6 @@ $(document).ready(function() {
 			var id = 'p' + i;
 			playerFBIds[id] = parsedResponse[i].user_fb_id;
 		}
-		window.alert("p1 = " + playerFBIds['p1'] + "/n" + 
-					 "p2 = " + playerFBIds['p2'] + "/n" +
-					 "p3 = " + playerFBIds['p3']);
 
 		//TO DO: Check if there's already a BIG picture on database
 		FB.api('/fql?q=SELECT%20src_big%20FROM%20photo%20WHERE%20pid%20IN%20%28SELECT%20pid%20FROM%20photo_tag%20WHERE%20subject%3D' + admin + '%20ORDER%20BY%20created%20ASC%29%20LIMIT%20100',  function(response) {
