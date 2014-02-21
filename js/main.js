0581ce68ab529ced020f6bb8d84b86c902fff427
Andy Au <au.andy.ch@gmail.com>
2014-02-21 13:00:48 -0500

    Andy : Reformatted code following Crockford's conventions.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var isFirstTimeLoop = true;
-var gameStarted = false;
-var FBId;
-var playerFBIds = {};
-var numCaptions;
-var numPlayers;
-var looper;
+var FBID;
+var FIRST_TIME_LOOP = true;
+var GAME_STARTED = false;
+var LOOPER;
+var NUM_CAPTIONS;
+var NUM_PLAYERS;
+var PLAYER_FB_IDS = {};
 
 $(document).ready(function() {
 	window.fbAsyncInit = function() {
@@ -29,28 +29,20 @@ $(document).ready(function() {
 
 	
 
-	// Main looper for every second
-	function myLooper()
-	{
-		$.post("services.php",
-		{
-			user : "all",
-		},function(response)
-		{
+	// Main LOOPER for every second
+	function myLooper() {
+		$.post("services.php", { user : "all"}, function(response) {
 			var parsedResponse = $.parseJSON(response);
 			var newNumPlayers = parsedResponse.length;
-			if (isFirstTimeLoop) // Fade in all if it's first time
-			{
-				isFirstTimeLoop = false;
-				numPlayers =  newNumPlayers;
-				for (var i = 0; i < newNumPlayers; i++)
-				{
+			if (FIRST_TIME_LOOP) {
+				FIRST_TIME_LOOP = false;
+				NUM_PLAYERS =  newNumPlayers;
+				for (var i = 0; i < newNumPlayers; i++) {
 					$('#p' + i + ' #name').text(parsedResponse[i].name); //name
 					$('#p' + i + ' img').attr('src', parsedResponse[i].fb_pp); // profile pic
 					$('#p' + i + ' img').attr('height', '85px');
 					$('#p' + i + ' img').attr('width', '85px');
-					if (i == 0) 
-					{
+					if (i == 0) {
 						$('#p' + i + ' p').text('======ADMIN======'); //caption for the admin
 					}
 					$('#p' + i + ' #score').text(parsedResponse[i].scores);//score
@@ -60,115 +52,101 @@ $(document).ready(function() {
 					$('#p' + i + ' img').hide().fadeIn(4000);
 					$('#p' + i + ' #score').hide().fadeIn(4000);
 				}
-			} 
-			else 
-			{
-				if (newNumPlayers == 4 && !gameStarted) // when 4 players have joined (start game)
-				{
+			} else {
+				if (newNumPlayers == 4 && !GAME_STARTED) { 
+					// 4 players have joined (start game)
 					startGame(parsedResponse);
-					gameStarted = true;
-				}		
-				if (newNumPlayers < 4 && gameStarted) // remove name/pic/etc when there are less players
-				{
-					gameStarted = false;
-					for (var i = 3; i >= newNumPlayers; i--)
-					{
+					GAME_STARTED = true;
+				} else if (newNumPlayers < 4 && GAME_STARTED) {
+					// remove name/pic/etc when there are less players
+					GAME_STARTED = false;
+					for (var i = 3; i >= newNumPlayers; i--) {
 						$('#p' + i + ' #name').hide();
 						$('#p' + i + ' img').hide();
 						$('#p' + i + ' p').hide();
 						$('#p' + i + ' #score').hide();
 					}
 				}
-				if (newNumPlayers > numPlayers) // if there are newly joined players
-				{
-					for (var i = numPlayers; i < newNumPlayers; i++)
-					{
+				if (newNumPlayers > NUM_PLAYERS) {
+					// if there are newly joined players
+					for (var i = NUM_PLAYERS; i < newNumPlayers; i++) {
 						// Fade in
 						$('#p' + i + ' #name').hide().fadeIn(4000);
 						$('#p' + i + ' img').hide().fadeIn(4000);
 					}
 				}
-				for (var i = 0; i < newNumPlayers; i++)
-				{
-					playerFBIds['p' + i] = parsedResponse[i].user_fb_id; //save players fb Ids
+				for (var i = 0; i < newNumPlayers; i++) {
+					PLAYER_FB_IDS['p' + i] = parsedResponse[i].user_fb_id; //player FB Ids
 
 					$('#p' + i + ' #name').text(parsedResponse[i].name).show(); //name
 					//profile pic
 					$('#p' + i + ' img').attr('src', parsedResponse[i].fb_pp).show();
 					$('#p' + i + ' img').attr('height', '85px');
 					$('#p' + i + ' img').attr('width', '85px');
-					if (i == 0) 
-					{
+					if (i == 0) {
 						$('#p' + i + ' p').text('======ADMIN======').show(); //caption for the admin
 					} else {
 						$('#p' + i + ' p').text(parsedResponse[i].caption_text).show();//caption
-						if (parsedResponse[i].caption_text !== "") numCaptions++;
+						if (parsedResponse[i].caption_text !== "") NUM_CAPTIONS++;
 					}
 					$('#p' + i + ' #score').text(parsedResponse[i].scores).show();//score
 				}
 
-				console.log("Numcaptions = " + numCaptions)
-				if (numCaptions == 3) // When all 3 captions are submitted
-				{
+				console.log("NUM_CAPTIONS = " + NUM_CAPTIONS)
+				if (NUM_CAPTIONS == 3) {
+					// When all 3 captions are submitted
 					adminPickWinner();
 				}
 
-				numCaptions = 0; // reset number of captions
-				numPlayers = newNumPlayers;
+				NUM_CAPTIONS = 0; // reset number of captions
+				NUM_PLAYERS = newNumPlayers;
 			}
 		});
 	}
 
-	function adminPickWinner()
-	{
-		isFirstTimeLoop = true;
-		gameStarted = false;
-		numCaptions = 0;
-		console.log("clearing looper");
-		clearInterval(looper);
-		if (playerFBIds['p0'] == FBId)
-		{
+	function adminPickWinner() {
+		FIRST_TIME_LOOP = true;
+		GAME_STARTED = false;
+		NUM_CAPTIONS = 0;
+		console.log("clearing LOOPER");
+		clearInterval(LOOPER);
+		if (PLAYER_FB_IDS['p0'] == FBID) {
 			window.alert("Admin, click on the person you think has the best caption!");
-			$('.container.player').click(function (){
+			$('.container.player').click(function () {
 				var id = $(this).attr('id');
 				var score = parseInt($('#' + id + ' #score').text()) + 10;
 
 				window.alert($('#' + id + ' #name').text() + ' gets 10 points! \n He now has ' 
 					+ score + ' points!');
-				window.alert("score = " + score + "\n" + "fb_id = " + playerFBIds[id]);
+				window.alert("score = " + score + "\n" + "fb_id = " + PLAYER_FB_IDS[id]);
 
-				$.post("services.php",
-				{
-					caption: 'clear'
-				});
+				$.post("services.php", { caption: 'clear' }); //clear all captions
 
-				$.post("services.php",
-				{
-					'score': 10,
-					'fb_id': playerFBIds[id],
+				$.post("services.php", 
+				{ 
+					'score': 10, 
+					'fb_id': PLAYER_FB_IDS[id] 
 				}, function(response) {
-					setTimeout(function () 
-					{
-						console.log("restarting Looper");
-						//looper = setInterval(function(){myLooper()}, 1000);
+					setTimeout(function () {
+						console.log("restarting LOOPER");
+						//LOOPER = setInterval(function(){myLooper()}, 1000);
 					}, 5000);
 				});
 				
 			});
-		}
-		else
-		{
+		} else {
 			window.alert("It's time for admin to pick who the winner is!");
 		}
 	}
 
 	function Initialize() {
-		var userName, profilePic;
+		var userName;
+		var profilePic;
 
 		FB.api('/me', function(response) {
-			FBId = response.id;
+			FBID = response.id;
 			if(document.getElementById('fb_id'))
-			    document.getElementById('fb_id').value = FBId
+			    document.getElementById('fb_id').value = FBID;
 			userName = response.name;
 			
 			FB.api('/me/picture', function(response) {
@@ -177,32 +155,31 @@ $(document).ready(function() {
 				$.post("services.php",
 				{
 					user : "add",
-					fb_id : FBId,
+					fb_id : FBID,
 					fb_name : userName,
 					fb_pp : profilePic
-				},function(response)
-				{
+				},function(response) {
 					console.log("AddUser Response: " + response);
 				});
 			});
 	    });
 	    
-	    looper = setInterval(function(){myLooper()}, 1000);
+	    LOOPER = setInterval(function() { myLooper() }, 1000);
 	}
 
-	function startGame(parsedResponse)
-	{
-		console.log("starting game...");
+	function startGame(parsedResponse) {
+		console.log(" starting game...");
 
 		//TO DO: Check if there's already a BIG picture on database
-		FB.api('/fql?q=SELECT%20src_big%20FROM%20photo%20WHERE%20pid%20IN%20%28SELECT%20pid%20FROM%20photo_tag%20WHERE%20subject%3D' + parsedResponse[0].user_fb_id + '%20ORDER%20BY%20created%20ASC%29%20LIMIT%20100',  function(response) {
-			var url = response.data[Math.floor((Math.random()*response.data.length - 1)+1)].src_big;
+		FB.api('/fql?q=SELECT%20src_big%20FROM%20photo%20WHERE%20pid%20IN%20%28SELECT%20pid%20FROM%20photo_tag%20WHERE%20subject%3D' 
+				+ parsedResponse[0].user_fb_id + '%20ORDER%20BY%20created%20ASC%29%20LIMIT%20100', function(response) {
+			var url = response.data[Math.floor((Math.random()*response.data.length - 1) + 1)].src_big;
 			console.log(url);
 			$('#bigPic').attr('src', url);
 		});
 	}
 
-	(function(d){
+	(function(d) {
 	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
 	if (d.getElementById(id)) {return;}
 	js = d.createElement('script'); 
