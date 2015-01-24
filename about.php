3b9d65424967a77c2c13a6bb061ea1acdb716804
Michael Parrott <michael.parrott94@gmail.com>
2015-01-23 23:22:10 -0500

    Some about page updates.

--- about.php
+++ about.php
@@ -2,30 +2,30 @@
 <html lang="en">
  <head>
 	 <meta charset="utf-8">
-	 <title>What's CAP-IT?</title>
+	 <title>About Cap It</title>
 	 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-     <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+   <link rel="stylesheet" type="text/css" href="css/styles.css"/>
  </head>
 
  <body>
     <div class="container"> 
 	    <dl>
-            <dt style="width:580px;" class="textColour">What's Cap-It?</dt>
-	        <dd class="textColour" >Cap-It is the product of an epic morphism between Cards Against Humanity, Draw Something, and Words with Friends.</dd>
+          <dt style="width:580px;" class="textColour">What's Cap-It?</dt>
+	          <dd class="textColour" >Cap-It is the product of an epic morphism between Cards Against Humanity, Draw Something, and Words with Friends.</dd>
 
 	        <dt style="width:580px; " class="textColour">How Do You Play?</dt>
-	        <dd class="textColour" >Start by finding 3 other friends that all have stupid and jokes pictures on Facebook. Then start a new game of CAP-IT!</dd>
-	        <dd class="textColour" >Each round, a random silly picture will appear (with atleast one of you in it), 3 of you will need to come up with a snappy caption</dd>
-	        <dd class="textColour" >and the "judge" will pick the best one. Just think cards against humanity, except people will be laughing at how stupid you looked all those years ago...</dd>
+	          <dd class="textColour" >Start by finding 3 other friends that all have stupid and jokes pictures on Facebook. Then start a new game of CAP-IT!</dd>
+	          <dd class="textColour" >Each round, a random silly picture will appear (with atleast one of you in it), 3 of you will need to come up with a snappy caption</dd>
+	          <dd class="textColour" >and the "judge" will pick the best one. Just think cards against humanity, except people will be laughing at how stupid you looked all those years ago...</dd>
 	        <dt style="width:580px; " class="textColour">Implementation!</dt>
-	        <dd class="textColour" >The user interface was built with HTML incorporated with CSS and Twitter Bootstrap along with javascript and Jquery to make the UI dynamic.</dd>
-	        <dd class="textColour" >The back was built with php and javascript on top of a postgres sever to host data. Furthermore there was use of the facebook graph API as well as the </dd>
-	        <dd class="textColour" >javascript sdk. All of this lead to an app hosted by heroku to bring friends closer together!</dd>
+	          <dd class="textColour" >The user interface was built with HTML incorporated with CSS and Twitter Bootstrap along with javascript and Jquery to make the UI dynamic.</dd>
+	          <dd class="textColour" >The back was built with php and javascript on top of a postgres sever to host data. Furthermore there was use of the facebook graph API as well as the </dd>
+	          <dd class="textColour" >javascript sdk. All of this lead to an app hosted by heroku to bring friends closer together!</dd>
 	        <dt style="width:580px; " class="textColour">The Team.</dt>
-	        <dd class="textColour" >Michael J. "The Answer" Parrott</dd>
-	        <dd class="textColour" >Andy "Piggy" Au</dd>
-	        <dd class="textColour" >Samuel "Wing Man" Yuen</dd>
-	        <dd class="textColour" >Si Te "King of the Crisp" Feng</dd>
+	          <dd class="textColour" >Michael Parrott</dd>
+	          <dd class="textColour" >Andy Au</dd>
+	          <dd class="textColour" >Samuel Yuen</dd>
+	          <dd class="textColour" >Si Te Feng</dd>
         </dl>
     </div>
  </body>
